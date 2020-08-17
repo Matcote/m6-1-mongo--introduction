cdf240756a68ab25e7fe1677733dc870aaef205d
Matcote <matthewcotesipod@gmail.com>
2020-08-17 15:01:51 -0400

    ex 2.3

--- exercises/exercise-2.js
+++ exercises/exercise-2.js
@@ -30,4 +30,18 @@ const createGreeting = async (req, res) => {
   client.close();
 };
 
-module.exports = { createGreeting };
+const getGreeting = async (req, res) => {
+  const _id = req.params._id;
+  const client = await MongoClient(MONGO_URI, options);
+  await client.connect();
+  const db = client.db("exercise_1");
+
+  db.collection("greetings").findOne({ _id }, (err, result) => {
+    result
+      ? res.status(200).json({ status: 200, _id, data: result })
+      : res.status(404).json({ status: 404, _id, data: "Not Found" });
+    client.close();
+  });
+};
+
+module.exports = { createGreeting, getGreeting };
